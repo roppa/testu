7767efe1386fb503eb573777e438793c8c39690f
Mark Robson <mark.o.robson@gmail.com>
2018-02-10 19:52:43 +0000

    Refactor for testability

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./lib/harness');
+const harness = require('./lib/harness');
+
+Promise.all(harness.tests)
+  .then((result) => {
+    harness.reporter.emit('end', harness.pass, harness.fail, harness.skipped);
+    if (harness.fail > 0) {
+      throw new Error(harness);
+    }
+  })
+  .catch((error) => {
+    harness.reporter.emit('end', harness.pass, harness.fail, harness.skipped);
+    process.exit(1);
+  });
+
+module.exports = harness.test;
